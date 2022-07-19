a1414ebe19e511a043e725c77cb25370eb9d9a72
Eunji Park <80046101+SavinPark@users.noreply.github.com>
2022-07-19 23:56:16 +0900

    상세페이지 SSR 적용

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import Head from 'next/head';
-import { Header, Divider } from 'semantic-ui-react';
+import { Header, Divider, Loader } from 'semantic-ui-react';
 import { useEffect, useState } from 'react';
 import ItemList from "../src/component/ItemList"
 
 export default function Home() {
-  const  [list, setList] = useState([]);
+  const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 
@@ -13,6 +14,7 @@ export default function Home() {
     axios.get(API_URL)
     .then(res => {
       setList(res.data);
+      setIsLoading(false);
     });
   }
 
@@ -25,16 +27,27 @@ export default function Home() {
       <Head>
         <title>코딩앙마 - Next.js 강좌</title>
       </Head>
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        베스트 상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(0, 9)} />
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        신상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(9)} />
+      {isLoading && (
+        <div style={{padding: "300px 0"}}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            베스트 상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(0, 9)} />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(9)} />
+        </>
+      )}
     </div>
   )
 }
--- src/component/Item.js
+++ src/component/Item.js
@@ -26,3 +26,5 @@ export default function Item({ item }) {
         </>
     )
 }
+
+
